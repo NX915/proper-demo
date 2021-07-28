499b6d4f7c37f39f5f76d99f2cae40238fc2b579
Sean Ni <nixiang915@hotmail.com>
2021-07-28 01:48:13 -0400

    add mock data export

--- src/Mock/property.ts
+++ src/Mock/property.ts
@@ -5,6 +5,7 @@ const propertyList = [
     class: 'Residential',
     type: 'Sale',
     daysOnMarket: 3,
+    askingPrice: 100000000,
     occupancy: 'Flexible',
     listDate: new Date(
       Date.now() - Math.floor(Math.random() * 2629743833)
@@ -20,8 +21,8 @@ const propertyList = [
       country: 'CA',
     },
     photoUrl: [
-      'https://du9bj9c2s4nh.cloudfront.net/wp-content/uploads/2020/09/TorontoRealEstateAugust.jpg',
-      'https://www.chicagotribune.com/resizer/2KEw8M19fOz8SX5oy8rUo2f5yto=/fit-in/800x533/smart/filters:fill(black)/arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/ZCJ77ZAR7FE2VBDQOTEC24ESSI.jpg',
+      'https://source.unsplash.com/random/?house',
+      'https://source.unsplash.com/random/?house,interior',
     ],
   },
   {
@@ -31,6 +32,7 @@ const propertyList = [
     type: 'Rent',
     daysOnMarket: 10,
     occupancy: 'Flexible',
+    askingPrice: 250000,
     listDate: new Date(
       Date.now() - Math.floor(Math.random() * 2629743833)
     ).toISOString(),
@@ -45,8 +47,34 @@ const propertyList = [
       country: 'CA',
     },
     photoUrl: [
-      'https://du9bj9c2s4nh.cloudfront.net/wp-content/uploads/2020/09/TorontoRealEstateAugust.jpg',
-      'https://www.chicagotribune.com/resizer/2KEw8M19fOz8SX5oy8rUo2f5yto=/fit-in/800x533/smart/filters:fill(black)/arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/ZCJ77ZAR7FE2VBDQOTEC24ESSI.jpg',
+      'https://source.unsplash.com/random/?house',
+      'https://source.unsplash.com/random/?house,interior',
+    ],
+  },
+  {
+    mlsNumber: 'M21463334',
+    status: 'U',
+    class: 'Commercial',
+    type: 'Lease',
+    daysOnMarket: 15,
+    occupancy: 'Flexible',
+    askingPrice: 900000,
+    listDate: new Date(
+      Date.now() - Math.floor(Math.random() * 2629743833)
+    ).toISOString(),
+    updatedOn: new Date(
+      Date.now() - Math.floor(Math.random() * 2629743833)
+    ).toISOString(),
+    address: {
+      street: '321 Test Street',
+      area: 'Spadina',
+      city: 'Toronto',
+      province: 'ON',
+      country: 'CA',
+    },
+    photoUrl: [
+      'https://source.unsplash.com/random/?house',
+      'https://source.unsplash.com/random/?house,interior',
     ],
   },
 ];
